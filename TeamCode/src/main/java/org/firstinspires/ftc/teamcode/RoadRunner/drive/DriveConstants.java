3d19780a443e05660d59de0444dfdfc5342dd31f
Elinora Melgarejo <96452829+ElinoraMelgarejo@users.noreply.github.com>
2022-12-30 15:10:06 -0500

    April Tags OpenCV

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
@@ -45,9 +45,11 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
+
     public static double WHEEL_RADIUS = 1.47638; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 15.33; // in
+
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
