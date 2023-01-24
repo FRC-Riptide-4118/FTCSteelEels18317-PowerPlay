fe522d94810553b7bd23491ab508fc8d9abb1641
lohanmrocha <88322645+lohanmrocha@users.noreply.github.com>
2023-01-24 18:44:08 -0500

    Update RRPathTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/RRPathTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/RRPathTest.java
@@ -1,6 +1,7 @@
-package org.firstinspires.ftc.teamcode.RoadRunner.drive;
+package orgpackage org.firstinspires.ftc.teamcode.RoadRunner.drive;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
@@ -12,20 +13,25 @@ public class RRPathTest extends LinearOpMode {
     @Override
 
     public void runOpMode() {
-        SampleMecanumDrive drivetrain = new SampleMecanumDrive(hardwareMap);
-
         waitForStart();
 
-        Trajectory testing = drivetrain.trajectoryBuilder(new Pose2d(4.4,-0.4, Math.toRadians(0)), Math.toRadians(0))
-                .splineToSplineHeading(new Pose2d(10.8,1.2, Math.toRadians(18)), Math.toRadians(18))
-                .splineToSplineHeading(new Pose2d(15.6,3.2, Math.toRadians(-8)), Math.toRadians(-8))
-                .splineToSplineHeading(new Pose2d(21.6,2.4, Math.toRadians(-30)), Math.toRadians(-30))
-                .splineToSplineHeading(new Pose2d(26.8,-0.4, Math.toRadians(-67)), Math.toRadians(-67))
-                .splineToSplineHeading(new Pose2d(28.4,-4.4, Math.toRadians(-86)), Math.toRadians(-86))
-                .splineToSplineHeading(new Pose2d(28.8,-9.6, Math.toRadians(-90)), Math.toRadians(-90))
-                .splineToSplineHeading(new Pose2d(29.2,-22.4, Math.toRadians(-81)), Math.toRadians(-81))
-                .build();
+            SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-        drivetrain.followTrajectory(testing);
+            // We want to start the bot at x: 10, y: -8, heading: 90 degrees
+            Pose2d startPose = new Pose2d(-36, -36, Math.toRadians(90));
+
+            drive.setPoseEstimate(startPose);
+
+            Trajectory traj1 = drive.trajectoryBuilder(startPose)
+                    .forward(5)
+                    .splineTo(new Vector2d(-31, -28), Math.toRadians(45))
+                    .build();
+
+            Trajectory traj2 = drive.trajectoryBuilder(traj1.end())
+                    .splineTo(new Vector2d(20, 9), Math.toRadians(45))
+                    .build();
+
+            drive.followTrajectory(traj1);
+        }
     }
-}
+
