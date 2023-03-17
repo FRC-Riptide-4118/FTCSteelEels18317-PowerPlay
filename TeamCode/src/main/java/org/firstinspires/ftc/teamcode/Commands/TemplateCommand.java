b0985d33984ac2c683c97e28ecabd095a07b6b7b
BlakeS10497 <blake.sanders10497@gmail.com>
2023-03-16 23:26:43 -0400

    Create TemplateCommand.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/TemplateCommand.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Commands;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Slides;
+
+public class TemplateCommand extends CommandBase {
+
+    // Subsystems go here
+
+
+    public TemplateCommand(Slides slides)
+    {
+
+    }
+
+    @Override
+    public void initialize()
+    {
+
+    }
+
+    @Override
+    public void execute()
+    {
+
+    }
+
+    @Override
+    public boolean isFinished()
+    {
+        return true; // FIXME
+    }
+
+}
