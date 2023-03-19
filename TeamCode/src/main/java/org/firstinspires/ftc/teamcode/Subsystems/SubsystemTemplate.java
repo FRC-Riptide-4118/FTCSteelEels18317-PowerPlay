1ea665424836a739d3b7b6281333d8c159352e2b
BlakeS10497 <blake.sanders10497@gmail.com>
2023-03-18 22:34:14 -0400

    Create SubsystemTemplate.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystems/SubsystemTemplate.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class SubsystemTemplate extends SubsystemBase {
+
+    // Motors, sensors, servos
+
+
+    public SubsystemTemplate(HardwareMap hardwareMap) {
+        // Get motors/sensors/servos from hardware map
+
+        // configure motors/sensors/servos (e.g., motor direction)
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    /* ---------------- Custom Functions ---------------- */
+
+}
