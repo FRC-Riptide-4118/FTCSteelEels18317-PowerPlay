dd0b7b9c8d27e32292670840a24fd2941070fe9f
BlakeS10497 <blake.sanders10497@gmail.com>
2023-04-10 13:45:09 -0400

    update PID coefficients

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/SampleMecanumDrive.java
@@ -45,8 +45,8 @@ import java.util.List;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(8, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(8, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(10, 0, 0.4);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(12, 0, 0.2);
 
     public static double LATERAL_MULTIPLIER = 1;
 
