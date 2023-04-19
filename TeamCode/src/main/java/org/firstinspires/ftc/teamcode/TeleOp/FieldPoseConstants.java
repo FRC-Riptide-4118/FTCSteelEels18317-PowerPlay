3af0f11b669ca93391dc60e47f469aafd2d38c2d
BlakeS10497 <blake.sanders10497@gmail.com>
2023-04-18 23:47:49 -0400

    Right auto pathing attempt
    need to tune in constants for right side of the field

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldPoseConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/FieldPoseConstants.java
@@ -26,9 +26,14 @@ public class FieldPoseConstants {
     {
         public static Pose2d startPose           = new Pose2d(-37, 63, Math.toRadians(270));
         public static Pose2d preLoadMidJunction  = new Pose2d(-33, 16, Math.toRadians(45));
+        public static Pose2d MidJunction         = new Pose2d(-35, 14, Math.toRadians(52));
         public static Pose2d stack               = new Pose2d(-67.5, 7.5, Math.toRadians(180));
         public static Pose2d middleTile          = new Pose2d(-45, 10, Math.toRadians(180));
         public static Pose2d closeMidJunction    = new Pose2d(-34, 17, Math.toRadians(38));
+
+        public static Pose2d parkLeft           = new Pose2d(-64, 11.9, Math.toRadians(0));
+        public static Pose2d parkMiddle         = new Pose2d(-43, 11.9, Math.toRadians(0));
+        public static Pose2d parkRight          = new Pose2d(-30, 11.9, Math.toRadians(180));
     }
 
 }
